e04de9276195c86d246e6163e990f33707b9cde0
Jack-0ut <yugin12s@gmail.com>
2022-05-05 10:27:58 +0300

    Created class Book

---
+++ Global.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace BookStore
+{
+    public class Book
+    {
+        /*int id;
+        string name;
+        int quantity;
+        string ageRange;
+        double price;*/
+        public Book(int id,string name,int quantity,string ageRange,double price)
+        {
+            this.id = id;
+            this.name = name;
+            this.quantity = quantity;
+            this.ageRange = ageRange;
+            this.price = price;
+        }
+
+        public int id { get; private set; }
+        public string name { get; private set; }
+        public int quantity { get; private set; }
+        public string ageRange { get; private set; }
+        public double price { get; private set; }
+    }
+}
